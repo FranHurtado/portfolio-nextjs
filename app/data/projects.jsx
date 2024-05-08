3acf3427536ee5ffd73062bfb58bb4f4a036270a
Fran Hurtado <Fhursan@gmail.com>
2024-05-09 01:31:16 +0200

    Data for projects section

---
+++ app/data/projects.jsx
@@ -0,0 +1,50 @@
+const projects = [
+  {
+    title : 'Trokocity',
+    content : `  const project = {
+      "title": "Trokocity",
+      "description": "Website and mobile app",
+      "url": "https://negocios.trokocity.com",
+      "company": "Monobo",
+      "position": "Fullstack Developer",
+      "tools": [
+        "Javascript", "Typescript",
+        "NodeJS", "ExpressJS",
+        "MongoDB","Flutter",
+        "AWS", "Holded API"
+      ]
+    }`
+  },
+  {
+    title : 'Bookolicos',
+    content : `  const project = {
+      "title": "Bookolicos",
+      "description": "Front and Back website",
+      "url": "https://bookolicos.com",
+      "company": "Monobo",
+      "position": "Fullstack Developer",
+      "tools": [
+        "Javascript", "Typescript",
+        "NodeJS", "ExpressJS",
+        "MongoDB","ReactJS",
+        "AWS", "Stripe API"
+      ]
+    }`
+  },
+  {
+    title : 'Grisely',
+    content : `  const project = {
+      "title": "Grisely",
+      "description": "Wordpress website",
+      "url": "https://griselymasajistas.com",
+      "company": "Monobo",
+      "position": "Fullstack Developer",
+      "tools": [
+        "Wordpress", "PHP",
+        "Javascript"
+      ]
+    }`
+  }
+];
+
+export default projects;
